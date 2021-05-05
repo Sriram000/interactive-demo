afb893dbca2c2cd35c60eead7b94487f84d051a1
sriram000 <srirammani14@gmail.com>
2021-05-05 15:03:35 +0530

    Improved, example.

--- src/components/example.js
+++ src/components/example.js
@@ -2,18 +2,19 @@ import React, { useState } from 'react';
 
 function Example() {
     // Declare a new state variable, which we'll call "count"
-    const [count, setCount] = useState(0);
-    const handleClick = () => {
-        // console.log(count);
-          setCount(count + 1);
+    const [state, setState] = useState({ count: 0 });
+    const addOne = () => {
+          setState({count: state.count + 1});
+    }
+    const addTwo = () => {
+        setState({count: state.count + 2});
     }
 
     return (
         <div>
-            <p>You clicked {count} times</p>
-            <button onClick={ handleClick }>
-            Click me
-            </button>
+            <p>You clicked { state.count } times</p>
+            <button onClick={ addOne }> Add one </button>
+            <button onClick={ addTwo }> Add two </button>
         </div>
         );
     }
