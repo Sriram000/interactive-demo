976ffafc3ccae1160b74065a2491a04125f4ab30
sriram000 <srirammani14@gmail.com>
2021-05-07 15:54:42 +0530

    Removed some examples.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Calculator from './components/calculator.js';
-import Example from './components/example.js';
 
 function App() {
   return (
     <div className="App">
       { Calculator() }
-      { Example() }
     </div>
   );
 }
--- src/components/buttons.js
+++ src/components/buttons.js
@@ -9,16 +9,4 @@ const components = {
 const Buttons = (context, keys) =>
     keys.map((key) => components[typeof key](context, key));
 
-// const Buttons = (context, keys) => {
-
-//     return (
-//         <>
-//         { NumberButton(context, 1) }
-//         { NumberButton(context, 2) }
-//         { NumberButton(context, 3) } 
-//         { OperatorButton(context, '+') }
-//         </>
-//     );
-// }
-
 export default Buttons;
